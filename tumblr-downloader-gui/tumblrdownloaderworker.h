d3a0997356f50062c9f0c5b9f5e728fb2215dcd0
Fırat Salgür <firatsalgur@protonmail.com>
2017-02-25 23:09:37 +0000

    Add LGPLv3 License

--- tumblr-downloader-gui/tumblrdownloaderworker.h
+++ tumblr-downloader-gui/tumblrdownloaderworker.h
@@ -1,3 +1,23 @@
+/**
+ *  Tumblr Downloader is an application that authenticates to your Tumblr
+ *  account, and downloads all the liked photos to your computer.
+ *  Copyright (C) 2017  Firat Salgur
+
+ *  This file is part of Tumblr Downloader
+
+ *  Tumblr Downloader is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Tumblr Downloader is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Tumblr Downloader.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef TUMBLRDOWNLOADERWORKER_H
 #define TUMBLRDOWNLOADERWORKER_H
 
